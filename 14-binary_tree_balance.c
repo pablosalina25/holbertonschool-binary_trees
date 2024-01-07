284d40d08862e29c9022ff7db3de7df64ca67517
MarcosBremermann <mabm2004@gmail.com>
2024-01-07 20:34:08 -0300

    Task 14 v1.0

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_balance - function that measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: 0 if fail, balance of the binary tree if success
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    size_t left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (left_height - right_height);
+}
