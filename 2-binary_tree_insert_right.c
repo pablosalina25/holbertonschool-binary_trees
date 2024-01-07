c8159c115110ff456fecee694f7c375e1e4f3526
MarcosBremermann <mabm2004@gmail.com>
2024-01-06 21:48:45 -0300

    Task 2 v1.01: minor fixes

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
  *
  * Return: Pointer to the created node, or NULL on failure or if parent is NULL
 */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value):
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node;
 
