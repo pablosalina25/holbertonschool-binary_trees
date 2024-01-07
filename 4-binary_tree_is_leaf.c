31338ed9bf0e21b995d8a1e0e1467797f1eb128b
MarcosBremermann <mabm2004@gmail.com>
2024-01-07 00:52:39 -0300

    Task 4 v1.03: minor changes

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -6,7 +6,7 @@
  * 
  * Return: 0 if node == NULL, 1 if node is leaf
 */
-int binary_tree_is_root(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
     if (node == NULL)
         return (0);
