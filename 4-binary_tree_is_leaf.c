dc3dbf1225fcf6c8fa4d90bb350664ff55b86d77
pablosalina25 <pablosalina25@gmail.com>
2024-01-07 08:31:04 -0800

    betty fixed

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,13 +3,12 @@
 /**
  * binary_tree_is_root - function that checks if a node is a root
  * @node: pointer to node to check
- * 
  * Return: 0 if node == NULL, 1 if node is leaf
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-        return (0);
+	if (node == NULL)
+	return (0);
 
-    return (node->left == NULL && node->right == NULL);
+	return (node->left == NULL && node->right == NULL);
 }
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,17 +2,18 @@
 #include <stdlib.h>
 
 /**
- * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - function that goes through a binary tree
+ * using pre-order traversal
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
  * Return: nothing only if either tree or func are NULL
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
+	if (tree == NULL || func == NULL)
+	return;
 
-    func(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
