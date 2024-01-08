78a5e753dc8b6cb37a676df8504ee9a1c8a605ef
pablosalina25 <pablosalina25@gmail.com>
2024-01-08 08:46:02 -0800

    betty and task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -10,13 +10,13 @@
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    return (binary_tree_is_full(tree) && left_height == right_height);
+	return (binary_tree_is_full(tree) && left_height == right_height);
 }
