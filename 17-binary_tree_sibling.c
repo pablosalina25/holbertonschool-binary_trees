66146b02b7194983eb9dd0bb1a9c2efd774eb3b3
pablosalina25 <pablosalina25@gmail.com>
2024-01-08 16:18:42 -0800

    fixed 17 and betty again

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,11 +11,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 	return (NULL);
 
-	if (node->parent->left == node && node->parent->right != NULL)
-	return (node->parent->right);
-
-	if (node->parent->right == node && node->parent->left != NULL)
-	return (node->parent->left);
-
-	return (NULL);
+	if (node->parent->left == node
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
